5e440acc448397f2a35ce47f5635d9c291455193
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2025-04-14 12:37:44 -0300

    importações

--- NotasAlunos/src/Suap/abstractClass/UsuarioAbstract.java
+++ NotasAlunos/src/Suap/abstractClass/UsuarioAbstract.java
@@ -1,6 +1,10 @@
 
 package Suap.abstractClass;
-import Suap.conexao;
+import Suap.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
         
 /**
  *
