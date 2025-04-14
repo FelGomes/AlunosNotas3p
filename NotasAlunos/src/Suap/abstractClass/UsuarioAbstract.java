a128a0c1cc38a31b2207077dec786a9cecdd3f9d
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2025-04-14 12:18:09 -0300

    Update UsuarioAbstract.java

--- NotasAlunos/src/Suap/abstractClass/UsuarioAbstract.java
+++ NotasAlunos/src/Suap/abstractClass/UsuarioAbstract.java
@@ -5,6 +5,49 @@ package Suap.abstractClass;
  *
  * @author Carlos
  */
-public class UsuarioAbstract {
+public abstract class UsuarioAbstract {
+    
+    private String cpf, endereco, sexo, dataNascimento;
+    private int id;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
     
 }
