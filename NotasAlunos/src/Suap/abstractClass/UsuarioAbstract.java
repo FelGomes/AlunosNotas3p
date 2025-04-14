bc8fe3534710fe04fd3f48c77d5de1da0a47d454
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2025-04-14 12:35:27 -0300

    Colocando metodo de inserir no banco de dados

--- NotasAlunos/src/Suap/abstractClass/UsuarioAbstract.java
+++ NotasAlunos/src/Suap/abstractClass/UsuarioAbstract.java
@@ -1,15 +1,24 @@
 
 package Suap.abstractClass;
-
+import Suap.conexao;
+        
 /**
  *
  * @author Carlos
  * @since 14-04 at 12:21
+ * 
  */
 public abstract class UsuarioAbstract {
     
-    private String cpf, endereco, sexo, dataNascimento;
-    private int id;
+    private String nome, cpf, endereco, sexo, dataNascimento;
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+     public String getNome() {
+        return nome;
+    }
 
     public String getCpf() {
         return cpf;
@@ -42,13 +51,31 @@ public abstract class UsuarioAbstract {
     public void setDataNascimento(String dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
+    
+    
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+    
+    
+    /**
+     * Método para inserir na tabela Usuario
+     * @throws SQLException 
+     */
+    public void inserir() throws SQLException{
+        Connection conexao = new Conexao().getConexao();
+        String sql = "Insert into usuario values (nome, endereco, cpf, sexo, dataNascimento) values (?,?,?,?,?)";
+        try {
+            PreparedStatement stmt = null;
+            stmt.setString(1, this.getNome());
+            stmt.setString(2, this.getEndereco());
+            stmt.setString(3, this.getCpf());
+            stmt.setString(4, this.getSexo());
+            stmt.setString(5, this.getDataNascimento());
+        } catch (Exception e) {
+            System.out.println("Erro ao fazer a inserção de dados no Banco! " + e.getMessage());
+        }
+        
+            
+            
     }
     
 }
