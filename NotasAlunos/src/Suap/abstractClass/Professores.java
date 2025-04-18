16c8312ebce55e01d90cd37ec0efc6d45fc4db41
Dheniel Rodrigues Luis <dheniel.luis@estudante.ifgoiano.edu.br>
2025-04-18 20:52:10 -0300

    Criação de classe professores
    Criação dos atributos da Classe professor, bem como criação dos Getters e Setters básicos para o funcionamento. Além da importação para implementação de métodos futuros

---
+++ NotasAlunos/src/Suap/abstractClass/Professores.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Suap.abstractClass;
+
+import Suap.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Dheniel Rodrigues Luis 
+ * @since 18/04/2025 as 20:38h
+ */
+public class Professores extends UsuarioAbstract {
+    
+    private int idProfessor;
+    private String disciplinaMinistrada;
+    private String turmaEnsinada;
+    private String grauTitularidade;
+
+    public int getIdProfessor() {
+        return idProfessor;
+    }
+
+    public void setIdProfessor(int idProfessor) {
+        this.idProfessor = idProfessor;
+    }
+
+    public String getDisciplinaMinistrada() {
+        return disciplinaMinistrada;
+    }
+
+    public void setDisciplinaMinistrada(String disciplinaMinistrada) {
+        this.disciplinaMinistrada = disciplinaMinistrada;
+    }
+
+    public String getTurmaEnsinada() {
+        return turmaEnsinada;
+    }
+
+    public void setTurmaEnsinada(String turmaEnsinada) {
+        this.turmaEnsinada = turmaEnsinada;
+    }
+
+    public String getGrauTitularidade() {
+        return grauTitularidade;
+    }
+
+    public void setGrauTitularidade(String grauTitularidade) {
+        this.grauTitularidade = grauTitularidade;
+    }
+    
+    
+}
