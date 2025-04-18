845275cc1df40b488ef2a9c9af9c520448b33558
Felipe Gomes <felipeferreiraag0@gmail.com>
2025-04-18 11:13:56 -0300

    Criando a Classe para Funções

---
+++ NotasAlunos/src/Moodle/MoodleApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Moodle;
+/**
+ *
+ * @author felipe
+ * @since 18-04 at 10:49 am
+ */
+public class MoodleApp {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
