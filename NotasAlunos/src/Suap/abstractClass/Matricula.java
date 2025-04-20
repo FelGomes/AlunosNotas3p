343711ba9d2f9417f0021ddbb938abf4e39add30
Kaua10710 <kaduzerah06@gmail.com>
2025-04-20 11:41:25 -0300

    Update Matricula.java
    getters e setters.

--- NotasAlunos/src/Suap/abstractClass/Matricula.java
+++ NotasAlunos/src/Suap/abstractClass/Matricula.java
@@ -14,4 +14,35 @@ public class Matricula {
     private String matricula_data;
     private int aluno_id;
     private int instituicao_id;
+    
+    public int getId(){
+        return matricula_id;
+    }
+    public void setId(int id){
+        this.matricula_id = id;
+    }
+    public String getData() {
+        return matricula_data;
+    }
+    public void setData(String data){
+        this.matricula_data = data;
+    }
+    public int getAlunoId(){
+        return aluno_id;
+    }
+    public void setAlunoId(int aluno_id){
+        this.aluno_id = aluno_id;
+    }
+    public int getInstituicaoId(){
+        return instituicao_id;
+    }
+    public void setInstituicaoId(int instituicao_id){
+        this.instituicao_id = instituicao_id;
+    }
+    
+    
+    
+    
+    
 }
+
