9d55295c7cc84e833e91fd42ad0ec048c956c4d6
Kaua10710 <kaduzerah06@gmail.com>
2025-04-20 11:33:48 -0300

    Create Matricula.java
    criação inicial da classe matricula.

---
+++ NotasAlunos/src/Suap/abstractClass/Matricula.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Suap.abstractClass;
+
+/**
+ *
+ * @author Kaua
+ * @since 20/04/2025 as 11:26
+ */
+public class Matricula {
+    private int matricula_id;
+    private String matricula_data;
+    private int aluno_id;
+    private int instituicao_id;
+}
