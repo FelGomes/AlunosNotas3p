f2dea79527fadd19f24c32cf4122d04ce640b734
Kaua10710 <kaduzerah06@gmail.com>
2025-04-30 16:04:12 -0300

    Reiniciando a classe Matricula para ficar bater os atributos baterem com o banco.

--- NotasAlunos/src/Suap/abstractClass/Matricula.java
+++ NotasAlunos/src/Suap/abstractClass/Matricula.java
@@ -14,137 +14,52 @@ import java.sql.ResultSet;
  * @since 20/04/2025 as 11:26
  */
 public class Matricula {
-    private int matricula_id;
-    private String data_matricula;
-    private int aluno_id;
-    private int instituicao_id;
-    private int curso_id;
+    private int matriculas_id;
+    private String matriculas_data_inicio;
+    private String matriculas_data_fim;
+    private int qtd_tempo;
+    private int fk_matricula_instituicao_id;
+    private int fk_matricula_alunos_;
     
-    public int getId(){
-        return matricula_id;
+    //Getters e Setters.
+    public int getMatriculaId(){
+        return matriculas_id;
     }
-    public void setId(int id){
-        this.matricula_id = id;
+    public void setMatriculasId(int matriculas_id){
+        this.matriculas_id = matriculas_id;
     }
-    public String getDataMatricula() {
-        return data_matricula;
+   public void setMatriculasDataInicio(String matriculas_data_inicio) {
+        this.matriculas_data_inicio = matriculas_data_inicio;
     }
-    public void setDataMatricula(String data){
-        this.data_matricula = data;
+   public String getMatriculasDataFim(){
+       return matriculas_data_fim;
+   }
+   public void setMatriculaDataFim(String matriculas_data_fim){
+       this.matriculas_data_fim = matriculas_data_fim;
+   }
+   public int getQtdTempo(){
+       return qtd_tempo;
+   }
+   public void setQtdTempo(int qtd_tempo){
+       this.qtd_tempo = qtd_tempo;
+   }
+   public int getFkInstituicaoId(){
+       return fk_matricula_instituicao_id;
+       
+   }
+   public void setFkinstituicaoId(int fk_matricula_instituicao_id){
+       this.fk_matricula_instituicao_id = fk_matricula_instituicao_id;
+   }
+  public int getFkAlunoId() {
+        return fk_matricula_alunos_;
     }
-    public int getAlunoId(){
-        return aluno_id;
-    }
-    public void setAlunoId(int aluno_id){
-        this.aluno_id = aluno_id;
-    }
-    public int getInstituicaoId(){
-        return instituicao_id;
-    }
-    public void setInstituicaoId(int instituicao_id){
-        this.instituicao_id = instituicao_id;
-    }
-     public int getCursoId() {
-        return curso_id;
-    }
-    public void setCursoId(int curso_id) {
-        this.curso_id = curso_id;
-    }
-    
-    public void inserir(){
-        String sql = "INSERT INTO matricula (matricula_data, aluno_id, instituicao_id) VALUES (?, ?, ?)";
-         try (Connection conexao = new Conexao().getConexao();
-             PreparedStatement stmt = conexao.prepareStatement(sql)) {
-            
-            stmt.setString(1, this.getDataMatricula());
-            stmt.setInt(2, this.getAlunoId());
-            stmt.setInt(3, this.getInstituicaoId());
-            stmt.setInt(4, this.getCursoId());
-            stmt.execute();
-            
-        } catch (SQLException e) {
-            System.out.println("Erro ao inserir matricula: " + e.getMessage());
-        }
-    }
-    
-    public void deletar(int matricula_id){
-         String sql = "DELETE FROM matricula WHERE matricula_id = ?";
-        try (Connection conexao = new Conexao().getConexao();
-             PreparedStatement stmt = conexao.prepareStatement(sql)) {
-            
-            stmt.setInt(1, matricula_id);
-            stmt.execute();
-            
-        } catch (SQLException e) {
-            System.out.println("Erro ao deletar matricula: " + e.getMessage());
-        }
-    }
-    public void alterar(int matricula_id){
-         String sql = "UPDATE matricula SET matricula_data = ?, aluno_id = ?, instituicao_id = ? WHERE matricula_id = ?";
-        try (Connection conexao = new Conexao().getConexao();
-             PreparedStatement stmt = conexao.prepareStatement(sql)) {
-            
-            stmt.setString(1, this.getDataMatricula());
-            stmt.setInt(2, this.getAlunoId());
-            stmt.setInt(3, this.getInstituicaoId());
-              stmt.setInt(4, this.getCursoId());
-             stmt.setInt(5, matricula_id);
-            stmt.execute();
-            
-        } catch (SQLException e) {
-            System.out.println("Erro ao alterar matricula: " + e.getMessage());
-        }
-    }
-    public void listar(int matricula_id) {
-         String sql = "SELECT m.matricula_id, m.matricula_data, " +
-                 "a.aluno_id, a.aluno_nome, " +
-                 "i.instituicao_id, i.instituicao_nome " +
-                 "FROM matricula m " +
-                 "INNER JOIN aluno a ON m.aluno_id = a.aluno_id " +
-                 "INNER JOIN instituicao i ON m.instituicao_id = i.instituicao_id";
-
-    if (matricula_id > 0) {
-        sql += " WHERE m.matricula_id = ?";
-    }
-
-    try (Connection conexao = new Conexao().getConexao();
-         PreparedStatement stmt = conexao.prepareStatement(sql)) {
-
-        if (matricula_id > 0) {
-            stmt.setInt(1, matricula_id);
-        }
-
-        ResultSet rs = stmt.executeQuery();
-
-        while (rs.next()) {
-            System.out.println("=======================================");
-            System.out.println("ID da Matrícula: " + rs.getInt("matricula_id"));
-            System.out.println("Data da Matrícula: " + rs.getString("matricula_data"));
-            System.out.println("Aluno: " + rs.getString("aluno_nome") + " (ID: " + rs.getInt("aluno_id") + ")");
-            System.out.println("Instituição: " + rs.getString("instituicao_nome") + " (ID: " + rs.getInt("instituicao_id") + ")");
-            System.out.println("=======================================");
-        }
-
-    } catch (SQLException e) {
-        System.out.println("Erro ao listar matrículas com JOIN: " + e.getMessage());
-    }
-}
-    
-    public boolean verificarMatricula(int matricula_id) {
-        String sql = "SELECT * FROM matricula WHERE matricula_id = ?";
-        try (Connection conexao = new Conexao().getConexao();
-             PreparedStatement stmt = conexao.prepareStatement(sql)) {
-            
-            stmt.setInt(1, matricula_id);
-            ResultSet rs = stmt.executeQuery();
-            return rs.next();
-            
-        } catch (SQLException e) {
-            System.out.println("Erro ao verificar matricula: " + e.getMessage());
-        }
-        return false;
-    }
-
+  public void setFkAlunoId(int fk_matricula_alunos_){
+      this.fk_matricula_alunos_ = fk_matricula_alunos_;
+  }
+  
+  
+  
+  
 }
 
 
