f1abe47672b6510b92ebe8d31d10dd81f27c8bac
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2025-06-07 19:03:40 -0300

    Criando as classes TelaCrud e TelaMain

---
+++ NotasAlunos/src/Telas/TelaCrud.java
@@ -0,0 +1,13 @@
+
+package Telas;
+import java.io.IOException;
+/**
+ *
+ * @author Carlos
+ */
+public class TelaCrud {
+    
+    public static void montarTelaInicial() throws IOException {
+        
+    }
+}
