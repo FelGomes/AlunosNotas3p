d42de5743ec06f4c1c0a693b881c8a34e33831fa
Kaua10710 <kaduzerah06@gmail.com>
2025-06-12 15:42:10 -0300

    Inserção de rótulos e iniciando campos de texto

--- NotasAlunos/src/Telas/TMatricula.java
+++ NotasAlunos/src/Telas/TMatricula.java
@@ -30,5 +30,31 @@ public class TMatricula {
         JLabel rotuloDataFim = new JLabel("Data Início:");
         rotuloDataFim.setBounds(30, 100, 80, 20);
         janela.add(rotuloDataFim);
+        
+        JLabel rotuloQtdTempo = new JLabel("Qtd Tempo:");
+        rotuloQtdTempo.setBounds(30, 140, 80, 20);
+        janela.add(rotuloQtdTempo);
+        
+        JLabel rotuloInstituicao = new JLabel("ID Instituição:");
+        rotuloInstituicao.setBounds(30, 180, 100, 20);
+        janela.add(rotuloInstituicao);
+        
+        JLabel rotuloAluno = new JLabel("ID Aluno:");
+        rotuloAluno.setBounds(30, 220, 80, 20);
+        janela.add(rotuloAluno);
+        
+        JLabel rotuloFiltro = new JLabel("Filtrar por ID:");
+        rotuloFiltro.setBounds(30, 250, 80, 20);
+        janela.add(rotuloFiltro);
+        
+        // CAMPOS DE TEXT
+        JTextField campoID = new JTextField();
+        campoID.setBounds(150, 20, 200, 20);
+        janela.add(campoID);
+        
+        JFormattedTextField campoDataInicio = new JFormattedTextField(mascaraData("##/##/####"));
+        campoDataInicio.setBounds(150, 60, 200, 20);
+        janela.add(campoDataInicio);        
+        
     }
 }
