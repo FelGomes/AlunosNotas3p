ebf6fd974d4aeee349570456033dad3c731f8608
dheniiel <dheniel.luis@estudante.ifgoiano.edu.br>
2025-06-13 19:38:49 -0300

    criação classe TelaBoletim

---
+++ NotasAlunos/src/Telas/TelaBoletim.java
@@ -0,0 +1,27 @@
+package Telas;
+
+import java.awt.Font;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+
+/**
+ *
+ * @author Dheniel 
+ */
+public class TelaBoletim {
+    
+}
