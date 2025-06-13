5a12f7a3ce48179499ff36f2e6bc5e760e43e289
dheniiel <dheniel.luis@estudante.ifgoiano.edu.br>
2025-06-13 19:41:26 -0300

    criaçao estrutura  telaboletim

--- NotasAlunos/src/Telas/TelaBoletim.java
+++ NotasAlunos/src/Telas/TelaBoletim.java
@@ -23,5 +23,26 @@ import java.awt.event.MouseEvent;
  * @author Dheniel 
  */
 public class TelaBoletim {
+    private static JTable tabelaBoletim;
+    private static DefaultTableModel modelBoletim;
+    
+    public static void montarTelaVisualizacao(){
+        final JFrame oJFrame = new JFrame("Boletim");
+        oJFrame.setBounds(450, 170, 580, 500);
+        oJFrame.setLayout(null);
+        JButton oJButtonBoletim = new JButton("Filtrar");
+        oJButtonBoletim.setBounds(420, 230, 100, 25);
+
+   
+        String[] colunasBoletim = {
+            "Nome", "Sexo", "Sala", "Turma", "Média", "Faltas", "Total Aulas"
+        };
+        
+        modelBoletim = new DefaultTableModel(colunasBoletim, 0); 
+        tabelaBoletim = new JTable(modelBoletim);
+        JScrollPane scrollBoletim = new JScrollPane(tabelaBoletim);
+        scrollBoletim.setBounds(20, 30, 530, 200); 
+        oJFrame.add(scrollBoletim);
+    }
     
 }
